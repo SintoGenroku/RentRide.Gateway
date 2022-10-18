1839a8fbca4eddc0c0c888f2bfd384c57546398b
SintoGenroku <borozda.a.s@gmail.com>
2022-10-18 16:22:32 +0300

    configure gateway

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,9 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors(options => options
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin());
+
+await app.UseOcelot();
 
 app.Run();
